a202133eb9f8e0dd51ea44ce563e539648851c4e
Victor Wibisono <devacto@gmail.com>
2013-11-16 15:27:12 +1100

    Added client class for Wordnet problem.

---
+++ pa01-wordnet/src/ac/victor/java/wordnet/Client.java
@@ -0,0 +1,14 @@
+package ac.victor.java.wordnet;
+
+public class Client {
+
+	/**
+	 * Client class which 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
